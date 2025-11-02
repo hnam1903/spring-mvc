2e3078e8a7cdd6a21b321dd4688ac9a0ec62018c
hnam1903 <hnam1903@gmail.com>
2025-11-02 22:15:07 +0700

    beans

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -1,13 +1,21 @@
 package vn.hoidanit.laptopshop;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
-
+	// lấy ra tất cả các beans
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+		
+		ApplicationContext abc = SpringApplication.run(LaptopshopApplication.class, args);
+		for(String s: abc.getBeanDefinitionNames())
+{
+			System.out.println(s);
+}
+
 	}
 
 }
