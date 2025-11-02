a59af3d4f9f8d6011e4f291885d5af5ab4140905
hnam1903 <hnam1903@gmail.com>
2025-11-02 23:20:52 +0700

    update-Controller

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,8 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello World for Spring boot";
+        return "Hello World for Spring boot!!!!!!!";
     }
     
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String password() {
+        return "Only admin can access this page";
+    }
     
 }
