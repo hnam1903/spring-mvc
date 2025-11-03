162084fb582d63ff281cd24c988494a2ddef7fcc
hnam1903 <hnam1903@gmail.com>
2025-11-03 16:49:35 +0700

    update-bootstrap

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,37 +2,34 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from Controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
     private UserService userService;
 
-    @Autowired
-    public UserController(UserService theUserService) {
-        userService = theUserService;
+    public UserController(UserService thService) {
+        userService = thService;
     }
 
-
-    @GetMapping("")
-    public String getHomePage() {
-        return "Hello from Controller";
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = userService.handleHello();
+        model.addAttribute("nam", test);
+        model.addAttribute("hoidanit", "spring mvc");
+        return "hello";
     }
 
-    @GetMapping("/s")
-    public String getHomePageSerivce() {
-        return userService.handleHello();
+    @RequestMapping("/admin/user")
+    public String toSignUser() {
+        return "admin/user/create";
     }
 }
+
+
